a247f764058e82e2b85d16e1715a4e8c1efac693
idmasse <idmasse@icloud.com>
2025-06-20 15:26:58 -0700

    code improvements

--- paybot.py
+++ paybot.py
@@ -1,162 +1,136 @@
-import undetected_chromedriver as webdriver
-import traceback
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from utils.email_utils import send_email
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from config import *
+from utils.selenium_setup import *
 
 def paybot():
     try:
-        driver = webdriver.Chrome()
+        driver = get_driver()
+
+        browser_wait = WebDriverWait(driver, 15)
+
+        def wait_clickable(by, value):
+            return browser_wait.until(EC.element_to_be_clickable((by, value)))
+        
+        def wait_visible(by, value):
+            return browser_wait.until(EC.visibility_of_element_located((by, value)))
+                
+        def wait_presence(by, value):
+            return browser_wait.until(EC.presence_of_element_located((by, value)))
+
+        def close_link_popup():
+            try:
+                logger.info("waiting for link verification popup to appear...")
+                link_verification_popup_btn = wait_visible(By.CSS_SELECTOR, '.LinkActionButton.LinkActionButton--icon.LinkVerificationHeader-cancelButton')
+                link_verification_popup_btn.click()
+            except NoSuchElementException:
+                logger.info("link verification popup not found, continuing")
 
         # nav to the login page for the specific shopify account
-        print('opening new browser window and navigating to shopify login page')
-        shopify_login_url = os.getenv('SHOPIFY_LOGIN_URL')
-        driver.get(shopify_login_url)
-        time.sleep(7)
+        logger.info('opening new browser window and navigating to shopify login page')
+        driver.get(SHOPIFY_LOGIN_URL)
 
-        # find the email element, input the email and submit the email form
-        print('entering shopify login email')
-        shopify_email_el = driver.find_element(By.ID, 'account_email')
-        shopify_email = os.getenv('SHOPIFY_EMAIL')
-        shopify_email_el.send_keys(shopify_email)
-        shopify_email_el.send_keys(Keys.RETURN)
-        time.sleep(5)
+        shopify_login_email_field = wait_clickable(By.ID, "account_email")
+        shopify_login_email_field.send_keys(SHOPIFY_EMAIL)
+        email_next_btn = wait_clickable(By.NAME, 'commit')
+        email_next_btn.click()
 
-        # find the password input field, input the password and submit the login form
-        print('entering shopify login password')
-        shopify_password_el = driver.find_element(By.ID, 'account_password')
-        shopify_password = os.getenv('SHOPIFY_PASSWORD')
-        shopify_password_el.send_keys(shopify_password)
-        shopify_password_el.send_keys(Keys.RETURN)
-        print('waiting for shopify page to load...')
-        time.sleep(5)
+        shopify_login_password_field = wait_clickable(By.ID, "account_password")
+        shopify_login_password_field.send_keys(SHOPIFY_PASSWORD)
+        password_next_btn = wait_clickable(By.NAME, 'commit')
+        password_next_btn.click()
 
         try:
-            remind_me_link_el = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'remind-me-later-link')))
-            print("'Remind me later' link found, clicking it")
+            remind_me_link_el = wait_presence(By.CLASS_NAME, 'remind-me-later-link')
+            logger.info("'Remind me later' link found, clicking it")
             remind_me_link_el.click()
-            time.sleep(30)
         except:
-            print("'Remind me later' link not found, proceeding to iframe")
+            logger.info("'Remind me later' link not found, proceeding to iframe")
 
-        iframe = driver.find_element(By.NAME, "app-iframe")
+        iframe = wait_presence(By.NAME, "app-iframe")
         driver.switch_to.frame(iframe)
-        time.sleep(1)
 
         # select all the orders
-        print("selecting all orders checkbox")
-        all_orders_checkbox_el = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div[1]/div/div[1]/table/thead/tr/th[1]/label/span')
+        logger.info("selecting all orders checkbox")
+        all_orders_checkbox_el = wait_clickable(By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div[1]/div/div[1]/table/thead/tr/th[1]/label/span')
         all_orders_checkbox_el.click()
-        time.sleep(1)
 
         # click the pay all button
-        print('clicking pay all button & waiting for stripe payment page to load')
-        pay_all_orders_button_el = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div[1]/div/div[1]/div/div/div/div[2]/button')
+        logger.info('clicking pay all button & waiting for stripe payment page to load')
+        pay_all_orders_button_el = wait_clickable(By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div[1]/div/div[1]/div/div/div/div[2]/button')
         pay_all_orders_button_el.click()
         time.sleep(5)
 
         # switch into Stripe window
-        print('switching focus to current window for stripe page')
+        logger.info('switching focus to current window for stripe page')
         current_window = driver.current_window_handle
         driver.switch_to.window(current_window)
 
         # fill out form on stripe page
-        print('filling email in stripe form')
-        stripe_email_el = driver.find_element(By.ID, 'email')
-        stripe_email = os.getenv('STRIPE_EMAIL')
-        stripe_email_el.send_keys(stripe_email)
-        time.sleep(4)
+        logger.info('filling email in stripe form')
+        stripe_email_el = wait_clickable(By.ID, 'email')
+        stripe_email_el.send_keys(STRIPE_EMAIL)
 
-        # close link verification popup
-        # link_verification_popup_el = driver.find_element(By.CLASS_NAME, "LinkVerificationHeader-cancelButton")
-        # link_verification_popup_el.click()
-        # time.sleep(3)
+        #close the "link pay" popup on the checkout page if it appears
+        close_link_popup()
 
-        print('filling name in stripe form')
-        stripe_name_el = driver.find_element(By.NAME, 'name')
-        stripe_name = os.getenv('STRIPE_NAME')
-        stripe_name_el.send_keys(stripe_name)
-        time.sleep(1)
+        logger.info('filling name in stripe form')
+        stripe_name_el = wait_clickable(By.NAME, 'name')
+        stripe_name_el.send_keys(STRIPE_NAME)
 
-        print('entering promo code')
-        stripe_promo_code_el = driver.find_element(By.ID, 'promotionCode')
-        stripe_promo_code = os.getenv('STRIPE_PROMO_CODE')
-        stripe_promo_code_el.send_keys(stripe_promo_code)
-        time.sleep(3)
+        logger.info('entering promo code')
+        stripe_promo_code_el = wait_clickable(By.ID, 'promotionCode')
+        stripe_promo_code_el.send_keys(STRIPE_PROMO_CODE)
 
         # apply promo code
-        print("clicking apply button for promo code")
-        stripe_promo_apply_el = driver.find_element(By.CLASS_NAME, 'PromotionCodeEntry-applyButton')
+        logger.info("clicking apply button for promo code")
+        stripe_promo_apply_el = wait_clickable(By.CLASS_NAME, 'PromotionCodeEntry-applyButton')
         stripe_promo_apply_el.click()
-        time.sleep(4)
 
-        print('filling card number in stripe form')
-        stripe_cardnumber_el = driver.find_element(By.ID, 'cardNumber')
-        stripe_cardnumber = os.getenv('STRIPE_CARD_NUMBER')
-        stripe_cardnumber_el.send_keys(stripe_cardnumber)
-        time.sleep(1)
+        logger.info('filling card number in stripe form')
+        stripe_cardnumber_el = wait_clickable(By.ID, 'cardNumber')
+        stripe_cardnumber_el.send_keys(STRIPE_CARD_NUMBER)
 
-        print('filling card expiration in stripe form')
-        stripe_card_expiry_el = driver.find_element(By.ID, 'cardExpiry')
-        stripe_card_expiry_num = os.getenv('STRIPE_CARD_EXP')
-        stripe_card_expiry_el.send_keys(stripe_card_expiry_num)
-        time.sleep(1)
+        logger.info('filling card expiration in stripe form')
+        stripe_card_expiry_el = wait_clickable(By.ID, 'cardExpiry')
+        stripe_card_expiry_el.send_keys(STRIPE_CARD_EXP)
 
-        print('filling cvc in stripe form')
-        stripe_card_cvc_el = driver.find_element(By.ID, 'cardCvc')
-        stripe_card_cvc_num = os.getenv('STRIPE_CSV')
-        stripe_card_cvc_el.send_keys(stripe_card_cvc_num)
-        time.sleep(1)
+        logger.info('filling cvc in stripe form')
+        stripe_card_cvc_el = wait_clickable(By.ID, 'cardCvc')
+        stripe_card_cvc_el.send_keys(STRIPE_CSV)
 
-        print('filling cardholder name in stripe form')
-        stripe_cardholder_name_el = driver.find_element(By.ID, 'billingName')
-        stripe_cardholder_name = os.getenv('STRIPE_NAME')
-        stripe_cardholder_name_el.send_keys(stripe_cardholder_name)
-        time.sleep(1)
+        logger.info('filling cardholder name in stripe form')
+        stripe_cardholder_name_el = wait_clickable(By.ID, 'billingName')
+        stripe_cardholder_name_el.send_keys(STRIPE_NAME)
 
-        print('filling address line1 in stripe form')
-        stripe_billing_address1_el = driver.find_element(By.ID, 'billingAddressLine1')
-        stripe_billing_address1 = os.getenv('STRIPE_ADDR_!')
-        stripe_billing_address1_el.send_keys(stripe_billing_address1)
-        time.sleep(1)
-
-        print('filling billing city in stripe form')
-        stripe_billing_city_el = driver.find_element(By.ID, 'billingLocality')
-        stripe_billing_city = os.getenv('STRIPE_CITY')
-        stripe_billing_city_el.send_keys(stripe_billing_city)
-        time.sleep(1)
-
-        print('filling zip code in stripe form')
-        stripe_billing_zip_el = driver.find_element(By.ID, 'billingPostalCode')
-        stripe_billing_zip = os.getenv('STRIPE_ZIP')
-        stripe_billing_zip_el.send_keys(stripe_billing_zip)
+        logger.info('filling address line1 in stripe form')
+        stripe_billing_address1_el = wait_clickable(By.ID, 'billingAddressLine1')
+        stripe_billing_address1_el.send_keys(STRIPE_ADDR_1)
         time.sleep(2)
 
+        logger.info('filling billing city in stripe form')
+        stripe_billing_city_el = wait_clickable(By.ID, 'billingLocality')
+        stripe_billing_city_el.send_keys(STRIPE_CITY)
+
+        logger.info('filling zip code in stripe form')
+        stripe_billing_zip_el = wait_clickable(By.ID, 'billingPostalCode')
+        stripe_billing_zip_el.send_keys(STRIPE_ZIP)
+
         try:
-            print('checking for stripepass box')
-            stripe_checkbox_el = driver.find_element(By.ID, 'enableStripePass')
+            logger.info('checking for stripepass box')
+            stripe_checkbox_el = wait_clickable(By.ID, 'enableStripePass')
             stripe_checkbox_el.click()
             time.sleep(5)
         except:
             pass
 
-        print('clicking pay button')
-        stripe_pay_button = driver.find_element(By.CLASS_NAME, 'SubmitButton')
+        logger.info('clicking pay button')
+        stripe_pay_button = wait_clickable(By.CLASS_NAME, 'SubmitButton')
         stripe_pay_button.click()
         time.sleep(18)
 
         send_email("Nolonger PayBot Ran Successfully",  f"Nolonger PayBot ran sucessfully.")
             
     except Exception as e:
-        print("paying for stripe failed")
+        logger.warning("paying for stripe failed")
         error_message = traceback.format_exc()
         send_email("Nolonger Bot Failed", error_message)
         
